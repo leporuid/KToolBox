7219c3f2b6dcc1f5ed186602da823999c1eb3309
Yu-Min Peng <leporuid@icloud.com>
2026-04-18 16:14:56 +0800

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,1 +1,76 @@
-{\n  "description": "KToolBox flake using uv2nix and pyproject-nix",\n  "inputs": {\n    "nixpkgs": {\n      "url": "github:NixOS/nixpkgs",\n      "inputs": {\n        "flake-compat": {\n          "url": "github:edolstra/flake-compat"\n        }\n      }\n    },\n    "uv2nix": {\n      "url": "github:my/uv2nix"\n    },\n    "pyproject-nix": {\n      "url": "github:my/pyproject-nix"\n    }\n  },\n  "outputs": {\n    "packages": {\n      "default": "uv2nix.packages.default",\n      "ktoolbox": "uv2nix.packages.ktoolbox",\n      "ktoolbox-app": "uv2nix.packages.ktoolbox-app"\n    },\n    "apps": {\n      "default": "ktoolbox-app",\n      "ktoolbox": "uv2nix.apps.ktoolbox"\n    },\n    "devShells": {\n      "default": {\n        "packages": [\n          "packages.ktoolbox",\n          "packages.ktoolbox-app",\n          "python311"\n        ]\n      }\n    }\n  }\n}
+{
+  description = "KToolBox packaged with uv2nix";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    pyproject-nix = {
+      url = "github:pyproject-nix/pyproject.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    uv2nix = {
+      url = "github:pyproject-nix/uv2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, pyproject-nix, uv2nix, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        python = pkgs.python311;
+
+        workspace = uv2nix.lib.workspace.loadWorkspace {
+          workspaceRoot = ./.;
+        };
+
+        overlay = workspace.mkPyprojectOverlay {
+          sourcePreference = "wheel";
+        };
+
+        editableOverlay = workspace.mkEditablePyprojectOverlay {
+          root = ./.;
+        };
+
+        pythonSet = pkgs.callPackage pyproject-nix.build.packages {
+          inherit python;
+        };
+
+        finalPythonSet = pythonSet.overrideScope' (pkgs.lib.composeManyExtensions [
+          overlay
+          editableOverlay
+        ]);
+
+        ktoolboxPkg = finalPythonSet.ktoolbox;
+
+        ktoolboxApp = pkgs.writeShellApplication {
+          name = "ktoolbox";
+          runtimeInputs = [ ktoolboxPkg ];
+          text = ''
+            exec ${ktoolboxPkg}/bin/ktoolbox "$@"
+          '';
+        };
+      in {
+        packages = {
+          default = ktoolboxPkg;
+          ktoolbox = ktoolboxPkg;
+          ktoolbox-app = ktoolboxApp;
+        };
+
+        apps = {
+          default = flake-utils.lib.mkApp { drv = ktoolboxApp; };
+          ktoolbox = flake-utils.lib.mkApp { drv = ktoolboxApp; };
+        };
+
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.uv
+            python
+          ];
+          inputsFrom = [ ktoolboxPkg ];
+        };
+      });
+}
