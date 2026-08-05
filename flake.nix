74bb8077b579278e20dd13b579349ea5ac18e98c
Yu-Min Peng <leporuid@icloud.com>
2026-08-05 22:50:54 +0800

    Add files via upload

---
+++ flake.nix
@@ -0,0 +1,82 @@
+{
+  description = "KToolBox — CLI tool for downloading posts from Kemono";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    pyproject-nix = {
+      url = "github:pyproject-nix/pyproject.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    uv2nix = {
+      url = "github:pyproject-nix/uv2nix";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    pyproject-build-systems = {
+      url = "github:pyproject-nix/build-system-pkgs";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.uv2nix.follows = "uv2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, pyproject-nix, uv2nix, pyproject-build-systems }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        python = pkgs.python312;
+
+        workspace = uv2nix.lib.workspace.loadWorkspace {
+          workspaceRoot = ./.;
+        };
+
+        overlay = workspace.mkPyprojectOverlay {
+          sourcePreference = "wheel";
+          dependencies = {
+    	  ktoolbox = [ "full" ];
+  	};
+        };
+
+        pythonSet = (pkgs.callPackage pyproject-nix.build.packages {
+          inherit python;
+        }).overrideScope (
+          pkgs.lib.composeManyExtensions [
+            pyproject-build-systems.overlays.default
+            overlay
+          ]
+        );
+
+        # If you want a virtualenv package:
+        runtimeEnv = pythonSet.mkVirtualEnv "ktoolbox" workspace.deps.default;
+      in
+      {
+        packages.default = runtimeEnv;
+        packages.ktoolbox = runtimeEnv;
+
+        apps.default = {
+          type = "app";
+          program = "${runtimeEnv}/bin/ktoolbox";
+        };
+
+        devShells.default = pkgs.mkShell {
+          packages = [
+            python
+            pkgs.uv
+          ];
+
+          env = {
+            UV_PYTHON_DOWNLOADS = "never";
+            UV_PYTHON = python.interpreter;
+          };
+
+          shellHook = ''
+            unset PYTHONPATH
+          '';
+        };
+      }
+    );
+}
